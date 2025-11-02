9b509d8c9eb276d39400b83450edd72c7aee821e
DanielG1298 <glaser.daniel1204@gmail.com>
2025-11-02 17:26:10 -0600

    finished WS

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,10 @@
 DROP TABLE IF EXISTS employees;
 
+/*employees table*/
+CREATE TABLE employees(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    birthday DATE NOT NULL,
+    salary INTEGER NOT NULL
 
+);
